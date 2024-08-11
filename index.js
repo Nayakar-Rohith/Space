ac2179e285955e29ed2033c2e22e28e41ed2797c
Nayakar-Rohith <nayakarrohith302@gmail.com>
2024-08-11 11:33:22 -0500

    this is a basic connection to server with node

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("helllo")
+const {parse}=require("csv-parse")
+const fs=require("fs")
+const keplerData=[]
+
+const habitablePlanets=(planet)=>{
+    return planet['koi_disposition']==='CONFIRMED' && 
+    planet['koi_insol']>0.36 && planet['koi_insol']<1.11 &&
+    planet['koi_prad']<1.6;
+}
+
+fs.createReadStream('kepler_data.csv')
+.pipe(parse({
+    comment:'#',
+    columns:true
+}))
+.on('data',(planet)=>{
+    if(habitablePlanets(planet)){
+        keplerData.push(planet)
+    }
+})
+.on('error',(error)=>{
+    console.log(error)
+})
+.on('end',()=>{
+    console.log(keplerData.length)
+})
